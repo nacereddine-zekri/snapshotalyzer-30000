5a0796dd187ad7d0917e197b51427ad1ce73e708
nacer-zekri <nacereddine.zekri@gmail.com>
2020-07-17 11:32:46 +0200

    HANDLING ERROR

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -105,7 +106,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -113,14 +118,18 @@ def stop_instances(project):
 @click.option('--project', default=None,
     help="only instances for project (tag Project:<name>)")
 
-def stop_instances(project):
+def start_instances(project):
     "Start EC2 instances"
 
     instances = filter_instances(project)
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -133,9 +142,20 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}...".format(v.id))
             v.create_snapshot(Description="Created by SNapshotAlyzer 30000")
+
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
 
     return
 
